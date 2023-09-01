37c569dbd4d5f6eb1b8d41114ffcd6f5ae9c3b63
Emptea <aryas1725@gmail.com>
2023-09-01 17:37:12 +0300

    Initial commit

---
+++ MDK-ARM/RTE/_sensors_i2c_uart/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'sensors_i2c_uart' 
+ * Target:  'sensors_i2c_uart' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f0xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
