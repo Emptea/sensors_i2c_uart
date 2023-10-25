ed99774914b53ecb5657e02efd5a63e9d1a1990a
Emptea <aryas1725@gmail.com>
2023-10-25 11:57:55 +0300

    i2c it added

--- Core/Src/i2c.c
+++ Core/Src/i2c.c
@@ -60,15 +60,19 @@ void MX_I2C1_Init(void)
   /* Peripheral clock enable */
   LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_I2C1);
 
+  /* I2C1 interrupt Init */
+  NVIC_SetPriority(I2C1_IRQn, 0);
+  NVIC_EnableIRQ(I2C1_IRQn);
+
   /* USER CODE BEGIN I2C1_Init 1 */
 
   /* USER CODE END I2C1_Init 1 */
 
   /** I2C Initialization
   */
+  LL_I2C_DisableClockStretching(I2C1);
   LL_I2C_DisableOwnAddress2(I2C1);
   LL_I2C_DisableGeneralCall(I2C1);
-  LL_I2C_DisableClockStretching(I2C1);
   I2C_InitStruct.PeripheralMode = LL_I2C_MODE_I2C;
   I2C_InitStruct.Timing = 0x40003EFF;
   I2C_InitStruct.AnalogFilter = LL_I2C_ANALOGFILTER_ENABLE;
@@ -77,7 +81,7 @@ void MX_I2C1_Init(void)
   I2C_InitStruct.TypeAcknowledge = LL_I2C_ACK;
   I2C_InitStruct.OwnAddrSize = LL_I2C_OWNADDRESS1_7BIT;
   LL_I2C_Init(I2C1, &I2C_InitStruct);
-  LL_I2C_DisableAutoEndMode(I2C1);
+  LL_I2C_EnableAutoEndMode(I2C1);
   LL_I2C_SetOwnAddress2(I2C1, 0, LL_I2C_OWNADDRESS2_NOMASK);
   /* USER CODE BEGIN I2C1_Init 2 */
 	
--- Core/Src/lm75bd.c
+++ Core/Src/lm75bd.c
@@ -6,23 +6,32 @@ static uint32_t lm75bd_read(uint8_t* data, uint32_t nbytes)
 	/*    - to the Slave with a 7-Bit SLAVE_OWN_ADDRESS                   */
 	/*    - with a auto stop condition generation when transmit all bytes */
 	uint32_t cnt = 0;
-	LL_I2C_HandleTransfer(I2C1, (LM75BD_ADDR << 1), LL_I2C_ADDRSLAVE_7BIT, nbytes, LL_I2C_MODE_SOFTEND, LL_I2C_GENERATE_START_READ);
-//	LL_I2C_GenerateStartCondition(I2C1);
-//	LL_I2C_TransmitData8(I2C1, LM75BD_ADDR | LL_I2C_REQUEST_WRITE);
-//	while(!LL_I2C_IsActiveFlag_ADDR(I2C1)){};
-//  LL_I2C_ClearFlag_ADDR(I2C1);
-//	while(!LL_I2C_IsActiveFlag_TXE(I2C1)){};
-//  LL_I2C_GenerateStartCondition(I2C1);
-//  while(!LL_I2C_IsActiveFlag_SB(I2C1)){};
-//  LL_I2C_TransmitData8(I2C1, SLAVE_OWN_ADDRESS | I2C_REQUEST_READ);
-//  while (!LL_I2C_IsActiveFlag_ADDR(I2C1)){};
-//  LL_I2C_ClearFlag_ADDR(I2C1);
-	for (uint32_t i = 0; i < nbytes; i++)
-	{
-		while(LL_I2C_IsActiveFlag_RXNE(I2C1));
-		LL_I2C_ReceiveData8(I2C1);
-	}
+	LL_I2C_HandleTransfer(I2C1, (LM75BD_ADDR << 1), LL_I2C_ADDRSLAVE_7BIT, 2, LL_I2C_MODE_AUTOEND, LL_I2C_GENERATE_START_READ);
+//	LL_I2C_SetSlaveAddr(I2C1, (LM75BD_ADDR << 1));
+//	LL_I2C_SetTransferRequest(I2C1, LL_I2C_REQUEST_READ);
+//	LL_I2C_SetTransferSize(I2C1, nbytes);
+//	
+  LL_I2C_GenerateStartCondition(I2C1);
+	
+
+//	LL_I2C_AcknowledgeNextData(I2C1, LL_I2C_ACK);
+	while(LL_I2C_IsActiveFlag_RXNE(I2C1) & LL_I2C_IsActiveFlag_BUSY(I2C1));
+	*data++ = LL_I2C_ReceiveData8(I2C1);
+	
+	LL_I2C_AcknowledgeNextData(I2C1, LL_I2C_NACK);
+	while(LL_I2C_IsActiveFlag_RXNE(I2C1) & LL_I2C_IsActiveFlag_BUSY(I2C1));
+	*data++ = LL_I2C_ReceiveData8(I2C1);
+	
+	while(LL_I2C_IsActiveFlag_BUSY(I2C1));
 	LL_I2C_GenerateStopCondition(I2C1);
+	if(LL_I2C_IsActiveFlag_STOP(I2C1))
+  {
+    /* End of Transfer */
+    LL_I2C_ClearFlag_STOP(I2C1);
+	}
+	
+	//while(LL_I2C_IsActiveFlag_BUSY(I2C1));
+	//LL_I2C_GenerateStopCondition(I2C1);
 	return (cnt == nbytes);
 }
 
@@ -41,9 +50,9 @@ static int32_t two_complement_11bits(uint32_t num)
 
 float lm75bd_read_temp(void)
 {
-	uint32_t byte_temp = 0;
+	uint8_t byte_temp[2] = {0};
 	int32_t signed_temp = 0;
-	if (lm75bd_read((uint8_t *)byte_temp, 2)) signed_temp = two_complement_11bits(byte_temp);
+	if (lm75bd_read(byte_temp, 2)); //signed_temp = two_complement_11bits(byte_temp);
 	
 	return (float)(signed_temp*0.125f);
 }
