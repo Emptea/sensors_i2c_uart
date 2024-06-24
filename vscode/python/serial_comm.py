778c19bd8a8ffb208ee9c5fb9f05cef2893d8971
Emptea <aryas1725@gmail.com>
2024-06-24 16:16:09 +0300

    door handle check added

--- vscode/python/serial_comm.py
+++ vscode/python/serial_comm.py
@@ -37,6 +37,9 @@ data_ans_temp_format = format_header + format_f32 + 'H'
 data_ans_wetsens_sz = 20+6+2
 data_ans_wetsens_format = format_header + format_u8 + 'H'
 
+data_ans_handle_sz = 20 +6+6+2
+data_ans_handle_format = format_header + format_u16 + format_u16 + 'H'
+
 #whoami
 cmd_whoami = bytearray(20)
 struct.pack_into(format_header, cmd_whoami, 0, b'AURA', 1, own_addr, 0, 1, 0)
@@ -65,7 +68,6 @@ cmd_write_sens_1meas.extend(crc16(cmd_write_sens_1meas).to_bytes(2,'little'))
 cmd_write_sens_2meas = bytearray(36)
 struct.pack_into(format_header + format_f32 + format_f32, cmd_write_sens_2meas, 0, b'AURA', 1, 0, press_addr, 5, 16, 4, 7, 4, 5.5, 4, 7, 4, 0)
 cmd_write_sens_2meas.extend(crc16(cmd_write_sens_2meas).to_bytes(2,'little'))
-print(' '.join(format(x, '02x') for x in cmd_write_sens_2meas))
 
 def ask(cmd, ans_format, ans_sz):
     ser.write(cmd)
@@ -106,8 +108,11 @@ elif resp_whoami[9] == 5:
 elif resp_whoami[9] == 4:
     ask(cmd_data, data_ans_hum_press_format, data_ans_hum_press_sz)
     print('hum+temp sens')
+elif resp_whoami[9] == 7:
+    ask(cmd_data, data_ans_handle_format, data_ans_handle_sz)
+    print('door handle')
 else:
-    print('not a sens')
+    print('unrecognised device')
 
 # for i in range(10):
 #     if i%2 == 1:
