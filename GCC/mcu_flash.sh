dc3efd1727469ae92520530701a5d52a66c63e63
Emptea <aryas1725@gmail.com>
2024-07-02 11:45:51 +0300

    Create mcu_flash.sh

---
+++ GCC/mcu_flash.sh
@@ -0,0 +1,15 @@
+fileName=${PWD##*/}
+fileExt=".hex"
+fileNameFull=$filePath$fileName$fileExt
+echo "Download file: ${fileNameFull}"
+
+utility="C:\Program Files\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin\STM32_Programmer_CLI.exe"
+if "$utility" -c port=SWD -w $fileNameFull -s; then
+    python "./vscode/python/serial_comm.py" 
+    exit 0
+elif "$utility" -c port=USB1 -w $fileNameFull -s; then
+    python "./vscode/python/serial_comm.py"
+    exit 0
+else
+    exit 1
+fi
